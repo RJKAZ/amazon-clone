715d16ce8df754bf20809375000ca01ab6ada8c2
Richard Kazanowski <rjkaz@comcast.net>
2020-12-06 17:40:59 -0500

    update

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 making a clone of amazon
 
 - finished Section 2
+
+Left off on Section 3 - part 5 (Create React App)
+at the 13:38 mark  - made a mistake with copying and pasting the divs. 
---
+++ frontend/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import data from './';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">Amazona</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {data.products.map((product) => (
+              <div className="card">
+                <a href="product.html">
+                  <img className="medium" src="./images/p1.jpg" alt="product" />
+                </a>
+                <div className="card-body">
+                  <a href="product.html">
+                    <h2>Nike Slim Shirts</h2>
+                  </a>
+                  <div className="rating">
+                    <span><i className="fa fa-star"></i></span>
+                    <span><i className="fa fa-star"></i></span>
+                    <span><i className="fa fa-star"></i></span>
+                    <span><i className="fa fa-star"></i></span>
+                    <span><i className="fa fa-star"></i></span>
+                  </div>
+                  <div className="price"> $120</div>
+
+          ))
+        }
+
+          </div>
+              </div>
+      </div>
+
+
+      </main>
+      <footer className="row center">All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
