e1869a5ff653e20137dd9755f988ec9f2036e45a
Richard Kazanowski <rjkaz@comcast.net>
2020-12-24 21:19:54 -0500

    update

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,8 +1,12 @@
 import Axios from 'axios';
 import { CART_EMPTY } from '../constants/cartConstants';
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from "../constants/orderConstants"
+import { 
+  ORDER_CREATE_FAIL, 
+  ORDER_CREATE_REQUEST, 
+  ORDER_CREATE_SUCCESS,
+} from "../constants/orderConstants"
 
-export const CreateOrder = (order) => async (dispatch, getState) => {
+export const createOrder = (order) => async (dispatch, getState) => {
   dispatch({
    type: ORDER_CREATE_REQUEST, payload: order });
   try {
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,4 +1,9 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_RESET, ORDER_CREATE_SUCCESS } from "../constants/orderConstants"
+import { 
+  ORDER_CREATE_FAIL, 
+  ORDER_CREATE_REQUEST, 
+  ORDER_CREATE_RESET, 
+  ORDER_CREATE_SUCCESS 
+} from "../constants/orderConstants"
 
 export const orderCreateReducer = (state = {}, action) =>{
   switch(action.type){
